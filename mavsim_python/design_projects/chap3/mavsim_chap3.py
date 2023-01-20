b1f93a6f539376e8012732b318c450ad5ad2f846
davidcGIThub <davidc@linuxpopper.org>
2023-01-20 00:54:31 -0700

    updated data viewer

--- mavsim_python/design_projects/chap3/mavsim_chap3.py
+++ mavsim_python/design_projects/chap3/mavsim_chap3.py
@@ -14,7 +14,12 @@ from viewers.mav_viewer import MavViewer
 from viewers.data_viewer import DataViewer
 from message_types.msg_delta import MsgDelta
 from models.mav_dynamics_forces import MavDynamics
+import pyqtgraph as pg
+
 VIDEO = False
+PLOTS = True
+ANIMATION = True
+
 if VIDEO is True:
     from viewers.video_writer import VideoWriter
     video = VideoWriter(video_name="chap3_video.avi",
@@ -22,8 +27,11 @@ if VIDEO is True:
                         output_rate=SIM.ts_video)
 
 #initialize the viewers
-mav_view = MavViewer()  # initialize the mav viewer
-data_view = DataViewer()  # initialize view of data plots
+app = pg.QtWidgets.QApplication([]) # use the same main process for Qt applications
+if ANIMATION:
+    mav_view = MavViewer(app=app)  # initialize the mav viewer
+if PLOTS:
+    data_view = DataViewer(app=app)  # initialize view of data plots
 
 # initialize elements of the architecture
 mav = MavDynamics(SIM.ts_simulation)
@@ -31,11 +39,12 @@ delta = MsgDelta()
 
 # initialize the simulation time
 sim_time = SIM.start_time
+end_time = 60
 
 # main simulation loop
 print("Press Command-Q to exit...")
-while sim_time < SIM.end_time:
-    # -------vary forces and moments to check dynamics-------------
+while sim_time < end_time:
+    # ------- vary forces and moments to check dynamics -------------
     fx = 0  # 10
     fy = 0  # 10
     fz = 100  # 10
@@ -44,67 +53,26 @@ while sim_time < SIM.end_time:
     Mz = 0  # 0.1
     forces_moments = np.array([[fx, fy, fz, Mx, My, Mz]]).T
 
-    # -------physical system-------------
+    # ------- physical system -------------
     mav.update(forces_moments)  # propagate the MAV dynamics
 
-    # -------update viewer-------------
-    mav_view.update(mav.true_state)  # plot body of MAV
-    data_view.update(mav.true_state,  # true states
-                     mav.true_state,  # estimated states
-                     mav.true_state,  # commanded states
-                     delta,  # inputs to the aircraft
-                     SIM.ts_simulation)
+    # ------- update viewer -------------
+    if ANIMATION:
+        mav_view.update(mav.true_state)  # plot body of MAV
+    if PLOTS:
+        data_view.update(mav.true_state,  # true states
+                        mav.true_state,  # estimated states
+                        mav.true_state,  # commanded states
+                        delta,  # inputs to the aircraft
+                        SIM.ts_simulation)
     if VIDEO is True:
         video.update(sim_time)
 
-    # -------increment time-------------
+    # ------- increment time -------------
     sim_time += SIM.ts_simulation
+
+if PLOTS:
+    data_view.save_plot_image("ch3_plot")
 
 if VIDEO is True:
     video.close()
-
-
-
-# # initialize the visualization
-# mav_view = MavViewer()  # initialize the mav viewer
-
-# # initialize elements of the architecture
-# state = MsgState()
-
-# # initialize the simulation time
-# sim_time = SIM.start_time
-# motions_time = 0
-# time_per_motion = 3
-
-# # main simulation loop
-# while sim_time < SIM.end_time:
-#     # -------vary states to check viewer-------------
-#     if motions_time < time_per_motion:
-#         state.north += 10*SIM.ts_simulation
-#     elif motions_time < time_per_motion*2:
-#         state.east += 10*SIM.ts_simulation
-#     elif motions_time < time_per_motion*3:
-#         state.altitude += 10*SIM.ts_simulation
-#     elif motions_time < time_per_motion*4:
-#         state.psi += 0.1*SIM.ts_simulation
-#     elif motions_time < time_per_motion*5:
-#         state.theta += 0.1*SIM.ts_simulation
-#     else:
-#         state.phi += 0.1*SIM.ts_simulation
-#     # -------update viewer and video-------------
-#     mav_view.update(state)
-
-#     # -------increment time-------------
-#     sim_time += SIM.ts_simulation
-#     motions_time += SIM.ts_simulation
-#     if motions_time >= time_per_motion*6:
-#         motions_time = 0
-
-#     # -------update video---------------
-#     if VIDEO is True:
-#         video.update(sim_time)
-
-# if VIDEO is True:
-#     video.update(sim_time)
-
-# print("Press Ctrl-Q to exit...")
