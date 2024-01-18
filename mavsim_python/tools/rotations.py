02fc8608900c96c7c290fda557d1e48a2a7f9be0
Randy Beard <beard@byu.edu>
2024-01-18 16:03:53 -0700

    Update rotations.py - theta uses atan2 in quaterion_to_euler

--- mavsim_python/tools/rotations.py
+++ mavsim_python/tools/rotations.py
@@ -15,9 +15,9 @@ def quaternion_to_euler(quaternion):
     e2 = quaternion.item(2)
     e3 = quaternion.item(3)
     phi = np.arctan2(2.0 * (e0 * e1 + e2 * e3), e0**2.0 + e3**2.0 - e1**2.0 - e2**2.0)
-    theta = np.arcsin(2.0 * (e0 * e2 - e1 * e3))
+    # theta = np.arcsin(2.0 * (e0 * e2 - e1 * e3))
+    theta = -np.pi/2.0 + np.arctan2(np.sqrt(1+2.0*(e0*e2-e1*e3)), np.sqrt(1-2.0*(e0*e2-e1*e3)))
     psi = np.arctan2(2.0 * (e0 * e3 + e1 * e2), e0**2.0 + e1**2.0 - e2**2.0 - e3**2.0)
-
     return phi, theta, psi
 
 def euler_to_quaternion(phi, theta, psi):
